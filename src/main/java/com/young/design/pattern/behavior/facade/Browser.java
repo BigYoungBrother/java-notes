6e4c6f1df665797d6365eca983ff729f154cc076
Young <gaoyang8320@163.com>
2021-06-14 16:42:44 +0800

    外观模式

---
+++ src/main/java/com/young/design/pattern/behavior/facade/Browser.java
@@ -0,0 +1,18 @@
+package com.young.design.pattern.behavior.facade;
+
+/**
+ * 浏览器
+ *
+ * @author Young
+ * @Date 2021-06-14 16:30
+ */
+public class Browser {
+
+    public void open() {
+        System.out.println("我在打开浏览器");
+    }
+
+    public void close() {
+        System.out.println("我在关闭浏览器");
+    }
+}
---
+++ src/main/java/com/young/design/pattern/behavior/facade/Sign.java
@@ -0,0 +1,18 @@
+package com.young.design.pattern.behavior.facade;
+
+/**
+ * 打卡
+ *
+ * @author Young
+ * @Date 2021-06-14 16:27
+ */
+public class Sign {
+
+    public void signIn() {
+        System.out.println("我在签到");
+    }
+
+    public void signOut() {
+        System.out.println("我在签退");
+    }
+}
