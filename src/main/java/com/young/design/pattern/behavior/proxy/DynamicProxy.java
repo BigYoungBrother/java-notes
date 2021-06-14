279ef6d15f4db55c8c70c74229b3c23706d0fe27
Young <gaoyang8320@163.com>
2021-06-14 18:58:37 +0800

    动态代理

---
+++ src/main/java/com/young/design/pattern/behavior/proxy/DynamicProxy.java
@@ -0,0 +1,29 @@
+package com.young.design.pattern.behavior.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * 动态代理
+ *
+ * @author Young
+ * @Date 2021-06-14 18:54
+ */
+public class DynamicProxy implements InvocationHandler {
+    Object obj = null;
+
+    public Object newProxyInstance(Object realObj) {
+        this.obj = realObj;
+        Class<?> classType = realObj.getClass();
+        return Proxy.newProxyInstance(classType.getClassLoader(), classType.getInterfaces(), this);
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.print("I'm Proxy, I'm invoking...");
+        method.invoke(obj, args);
+        System.out.println("invoke end!");
+        return null;
+    }
+}
---
+++ src/main/java/com/young/design/pattern/behavior/proxy/DynamicProxyTest.java
@@ -0,0 +1,16 @@
+package com.young.design.pattern.behavior.proxy;
+
+/**
+ * 动态代理测试类
+ *
+ * @author Young
+ * @Date 2021-06-14 18:56
+ */
+public class DynamicProxyTest {
+
+    public static void main(String[] args) {
+        Subject subject = (Subject) new DynamicProxy().newProxyInstance(new RealSubject());
+        subject.operate();
+    }
+
+}
