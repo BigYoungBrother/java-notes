9ae3fb6f7008343c2e569ee6adbfe973e966dc36
Young <gaoyang8320@163.com>
2021-06-14 17:55:50 +0800

    享元模式

---
+++ src/main/java/com/young/design/pattern/behavior/flyweight/ShapeFactory.java
@@ -0,0 +1,24 @@
+package com.young.design.pattern.behavior.flyweight;
+
+import java.util.HashMap;
+
+/**
+ * 图形工厂
+ *
+ * @author Young
+ * @Date 2021-06-14 17:52
+ */
+public class ShapeFactory {
+
+    private static final HashMap<String, Shape> CIRCLE_MAP = new HashMap<>();
+
+    public static Shape getCircle(String color) {
+        Circle circle = (Circle) CIRCLE_MAP.get(color);
+        if(circle == null) {
+            circle = new Circle(color);
+            CIRCLE_MAP.put(color, circle);
+            System.out.println("Creating circle of color : " + color);
+        }
+        return circle;
+    }
+}
