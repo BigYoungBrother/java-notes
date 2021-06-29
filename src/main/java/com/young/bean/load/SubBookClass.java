7096640e8d65231f571835c05e74f3b5be2936bb
Young <gaoyang8320@163.com>
2021-06-29 22:58:27 +0800

    bean加载2

---
+++ src/main/java/com/young/bean/load/SubBookClass.java
@@ -0,0 +1,65 @@
+package com.young.bean.load;
+
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.context.EmbeddedValueResolverAware;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.MessageSource;
+import org.springframework.context.MessageSourceAware;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.util.StringValueResolver;
+
+/**
+ * SubBookClass 继承Book类
+ *
+ * @author Young
+ * @Date 2021-06-27 09:39
+ */
+public class SubBookClass extends Book implements BeanClassLoaderAware,
+        EnvironmentAware, EmbeddedValueResolverAware, ResourceLoaderAware,
+        ApplicationEventPublisherAware, MessageSourceAware {
+
+    private String bookSystem;
+
+    public String getBookSystem() {
+        return bookSystem;
+    }
+
+    public void setBookSystem(String bookSystem) {
+        System.out.println("设置BookSystem 的属性值");
+        this.bookSystem = bookSystem;
+    }
+
+    @Override
+    public void setBeanClassLoader(ClassLoader classLoader) {
+        System.out.println("SubBookClass.setBeanClassLoader() 方法被调用了");
+    }
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        System.out.println("SubBookClass.setApplicationEventPublisher() 方法被调用了");
+    }
+
+    @Override
+    public void setEmbeddedValueResolver(StringValueResolver resolver) {
+        System.out.println("SubBookClass.setEmbeddedValueResolver() 方法被调用了");
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        System.out.println("SubBookClass.setEnvironment() 方法被调用了");
+    }
+
+    @Override
+    public void setMessageSource(MessageSource messageSource) {
+        System.out.println("SubBookClass.setMessageSource() 方法被调用了");
+    }
+
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+        System.out.println("SubBookClass.setResourceLoader() 方法被调用了");
+    }
+}
